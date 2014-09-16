f6b09ee263fbc6cdd43098ebd1368c7da71e8a5f
Doug Tyler <dougwt@gmail.com>
2014-09-16 12:38:27 -0700

    Code cleanup

--- dweebooks.py
+++ dweebooks.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python
+"""
+TODO: Add documentation
+"""
 import datetime
 import glob
 import json
+import sys
 import time
+import logging
 from random import choice
 
 import tweepy
 
 
 class Dweebooks:
+    """TODO: One liner description."""
     def __init__(self, files):
         self.EOS = ['.', '?', '!']
         self.delay = 60 * 60
@@ -18,15 +25,21 @@ class Dweebooks:
         self.load_data(files)
         self.build_dict()
 
+        # TODO: add support for config files
         CONSUMER_KEY = 'JBBZYreWlks6WaUGGu46JNpnP'
         CONSUMER_SECRET = 'Yl4RROho8rKT7Y1axBjDO8Ot2DR3FiBSUDVbOoWz8ydC5YsIna'
         ACCESS_TOKEN = '2647029398-y6m8U3jgPyRLq4xl4xq4GgWku4IeeFpFLwhM7e0'
         ACCESS_TOKEN_SECRET = '10nEzYXsMmCcMjPEpjuxlWetTGBDxcv2z6PrJHik4iRCW'
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        self.api = tweepy.API(auth)
+        self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        self.auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        self.username = self.auth.get_username()
+        self.api = tweepy.API(self.auth)
+        self.listener = None
+        self.stream = None
 
-        log('Available seeds: %s' % len(self.seeds))
+        logging.basicConfig(filename=self.username+'.log', level=logging.DEBUG)
+
+        self.log('Available seeds: %s' % len(self.seeds))
 
     def load_data(self, files):
         """Load JSON data from tweet archives."""
@@ -40,7 +53,7 @@ class Dweebooks:
             for tweet in j:
                 tweets.append(tweet)
         self.tweets = sorted(tweets, key=lambda k: k['id'])
-        log('Found tweets: %s' % len(self.tweets))
+        self.log('Found tweets: %s' % len(self.tweets))
 
     def build_dict(self):
         """Build a Markov chain based on a list of tokens."""
@@ -73,115 +86,158 @@ class Dweebooks:
             try:
                 first, second = temp_tokens[0], temp_tokens[1]
                 self.seeds.append((first, second))
-                # log('Adding seed: (%s, %s)' % (first, second))
+                # self.log('Adding seed: (%s, %s)' % (first, second))
             except IndexError:
-                # log('Error analyzing seed: %s' % temp_tokens)
+                # self.log('Error analyzing seed: %s' % temp_tokens)
                 pass
 
-            # log('Adding tokens: %s' % temp_tokens)
-        log('Usable tweets: %s' % len(tokens))
+            # self.log('Adding tokens: %s' % temp_tokens)
+        self.log('Usable tweets: %s' % len(tokens))
         return tokens
 
-    def valid_token(self, token):
+    def valid_token(self, token, urls=False, mentions=True):
         """Determine whether a token should be placed in the dictionary."""
-        # if token.lower().startswith(u'http'):
-        #     log('Rejected token: %s' % token)
-        #     return False
-        if u'@' in token:
-            # log('Rejected token: %s' % token)
+        # Filter urls
+        if urls and token.lower().startswith(u'http'):
+            # self.log('Rejected token: %s' % token) TODO: debug log
             return False
+
+        # Filter mentions
+        if mentions and u'@' in token:
+            # self.log('Rejected token: %s' % token) TODO: debug log
+            return False
+
         return True
 
     def strip_token(self, token):
         """Remove unwanted characters from an individual token."""
         if token.startswith(u'"') or token.startswith(u"'") or token.startswith(u'('):
-            # log('Stripped token: %s' % token)
+            # self.log('Stripped token: %s' % token)
             token = token[1:]
         if token.endswith(u'"') or token.endswith(u"'") or token.endswith(u')'):
-            # log('Stripped token: %s' % token)
+            # self.log('Stripped token: %s' % token)
             token = token[:-1]
         return token
-
-    def generate_tweet_helper(self):
-        li = [key for key in self.dictionary.keys()]
-        key = choice(li)
-        # key = choice(self.seeds)
-
-        li = []
-        first, second = key
-        li.append(first)
-        li.append(second)
-        while True:
-            try:
-                third = choice(self.dictionary[key])
-            except KeyError:
-                break
-            li.append(third)
-            if len(third) < 1 or third[-1] in self.EOS:
-                break
-            # else
-            key = (second, third)
-            first, second = key
-
-        return ' '.join(li)
 
     def generate_tweet(self, max_length=140):
         """Generate pseudorandom tweets with a maximum length."""
         new_tweet = ''
         while True:
-            if len(new_tweet) < 25:
+            if len(new_tweet) < 25:            # tweet too short; extend it
                 if len(new_tweet) > 0:
-                    new_tweet += ' '
+                    new_tweet += ' '  # add space prefix for next substring
+                new_tweet += self.generate_markov_string()
+            elif len(new_tweet) > max_length:  # tweet too long; start over
 
-                new_tweet += self.generate_tweet_helper()
-            elif len(new_tweet) > max_length:
-                new_tweet = self.generate_tweet_helper()
-            else:
-                # found acceptable length tweet
+                new_tweet = self.generate_markov_string()
+            else:                              # acceptable-length tweet found
                 break
-
         return new_tweet
 
-    def check_mentions(self):
-        """Identify new incoming mentions."""
-        if self.most_recent_response_id:
-            mentions = self.api.mentions(since_id=self.most_recent_response_id)
-        else:
-            mentions = self.api.mentions()
-            self.most_recent_response_id = mentions[-1].id
-            mentions = []
+    def generate_markov_string(self):
+        """Generate a string based on the markov dictionary."""
+        # Choose a random key to start the chain
+        li = [key for key in self.dictionary.keys()]
+        key = choice(li)
 
-        for mention in mentions:
-            if mention.id > self.most_recent_response_id:
-                self.process_mention(mention.id)
-                self.most_recent_response_id = mention.id
+        # Add the chosen key and its successor to the string
+        first, second = key
+        li.append(first)
+        li.append(second)
 
-    def process_mention(self, id):
-        """Reply to a tweet of a given id."""
-        response_tweet = '@dougwt '
+        while True:
+            # lookup current key in dictionary
+            try:
+                third = choice(self.dictionary[key])
+            except KeyError:
+                # no successor found
+                # we've reached the end of this markov chain
+                break
 
-        length = 140 - len(response_tweet)
-        response_tweet += self.generate_tweet(max_length=length)
-        log('Simulated mention: %s' % response_tweet)
-        # self.api.update_status(response_tweet, in_reply_to_status_id=id)
+            # successor found, append and check for End-of-String symbols
+            li.append(third)
+            if len(third) < 1 or third[-1] in self.EOS:
+                break
+
+            # advance key to the next pair of tokens for the next loop
+            key = (second, third)
+            first, second = key
+
+        return ' '.join(li)
+
+    def process_mention(self, status):
+        """Reply to a mention status."""
+        # don't respond to tweets made by our own account
+        if status.user.screen_name == self.username:
+            return True
+
+        # prefix response with mention's username
+        new_tweet = '@%s ' % status.user.screen_name
+
+        # add any additional usernames mentioned in the tweet
+        for user in status.entities['user_mentions']:
+            if user['screen_name'] != self.username:  # don't include ourselves
+                new_tweet += '@%s ' % user['screen_name']
+
+        # generate the response text
+        new_tweet += self.generate_tweet(max_length=(140 - len(new_tweet)))
+
+        # make it happen, cap'n
+        self.api.update_status(new_tweet, status.id)
+        self.log('Tweeting: %s' % new_tweet)
 
     def start(self):
-        log('Starting dweebooks...')
+        """Start dweebooks bot."""
+        self.log('Starting dweebooks...')
+
+        # Setup async StreamListener to monitor mentions
+        self.listener = MentionListener(self)
+        self.stream = tweepy.Stream(self.auth, self.listener)
+        self.stream.userstream(_with='user', replies='all', async=True)
+
         while True:
-            # self.check_mentions()
+            new_tweet = self.generate_tweet()      # generate tweet text
+            self.api.update_status(new_tweet)      # publish tweet
+            self.log('Tweeting: %s' % new_tweet)   # update log
 
-            new_tweet = self.generate_tweet()
-            self.api.update_status(new_tweet)
-            log('Tweeting: %s' % new_tweet)
+            time.sleep(self.delay)                 # sleep until next tweet
 
-            time.sleep(self.delay)
+    def log(self, msg):
+        """Write a message to the dweebooks log."""
+        timestamp = datetime.datetime.fromtimestamp(
+            time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+        print u'[%s] %s' % (timestamp, msg)
+        sys.stdout.flush()
 
 
-def log(msg):
-    timestamp = datetime.datetime.fromtimestamp(
-        time.time()).strftime('%Y-%m-%d %H:%M:%S')
+class MentionListener(tweepy.StreamListener):
+    ''' Handles data received from the stream. '''
+    def __init__(self, bot, *args, **kwargs):
+        super(MentionListener, self).__init__(*args, **kwargs)
+        self.bot = bot
 
-    print '[%s] %s' % (timestamp, msg)
+    def on_status(self, status):
+        self.bot.log('Stream detected tweet: ' + status.text)
+        self.bot.process_mention(status)
+
+        return True  # To continue listening
+
+    def on_error(self, status_code):
+        self.bot.log('Got an error with status code: ' + str(status_code))
+
+        return True  # To continue listening
+
+    def on_timeout(self):
+        self.bot.log('Timeout...')
+
+        return True  # To continue listening
+
+    def on_friends(self, friends):
+        self.bot.log('Received friends list.')
+        self.bot.friends = friends
+
+        return True  # To continue listening
 
 
 def main():
